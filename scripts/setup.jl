a9a269c21844700145b67c85910f88ffb0e904fc
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2024-07-09 23:10:44 +0000

    Fix setup.jl

--- scripts/setup.jl
+++ scripts/setup.jl
@@ -1,5 +1,6 @@
 using Pkg
-Pkg; Pkg.Registry.add("General")
+
+Pkg.Registry.add("General")
 Pkg.Registry.add(RegistrySpec(url="https://github.com/tensor4all/T4ARegistry.git"))
 Pkg.instantiate()
 Pkg.build("IJulia")

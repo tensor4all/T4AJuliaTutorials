401c0e979ebccca9fed9aa56d2032343f21ac13e
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2024-09-08 13:32:08 +0900

    Add Conda packages conditionally based on operating system

--- scripts/setup.jl
+++ scripts/setup.jl
@@ -5,8 +5,14 @@ Pkg.instantiate()
 
 using Conda
 
-Conda.add("jupyter")
-Conda.add("jupytext")
-Conda.add("jupyter-book")
+if Sys.iswindows()
+	isfile(joinpath(Conda.PYTHONDIR, "jupyter.exe")) || Conda.add("jupyter")
+	isfile(joinpath(Conda.PYTHONDIR, "jupytext.exe")) || Conda.add("jupytext")
+	isfile(joinpath(Conda.PYTHONDIR, "jb.exe")) || Conda.add("jupyter-book")
+else
+	isfile(joinpath(Conda.PYTHONDIR, "jupyter")) || Conda.add("jupyter")
+	isfile(joinpath(Conda.PYTHONDIR, "jupytext")) || Conda.add("jupytext")
+	isfile(joinpath(Conda.PYTHONDIR, "jupyter-book")) || Conda.add("jupyter-book")
+end
 
 Pkg.build("IJulia")
